412f415037be8ae02937ae1bbb31ea6679484197
Andrew Holmes <andrewholmes@Andrews-MacBook-Pro.local>
2023-07-26 22:06:11 -0500

    remove and re-focus app to batch job role

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-import { infer } from "./services/chatService.js";
-// import https from 'https';
+import https from 'https';
 import { createInterface } from 'readline';
-
-infer("What is the name of the toyota of denton?");
 
 // let selection;
 
@@ -15,49 +12,9 @@ infer("What is the name of the toyota of denton?");
 //   selection = number;
 //   switch (selection) {
 //       case '0':
-//           // callYelp();
-//           infer("What is the name of the toyota of denton?");
+//           callYelp();
 //           break;
 //   }
   
 //   readline.close();
 // });
-
-function callYelp() {
-  const options = {
-      hostname: 'api.yelp.com',
-      path: '/v3/businesses/toyota-of-denton-denton-3/reviews',
-      method: 'GET',
-      headers: {
-          Authorization: `Bearer ${process.env.API_Key}`,
-          'Accept': 'application/json'
-      },
-  };
-    
-  const req = https.request(options, (res) => {
-      let data = '';
-    
-      res.on('data', (chunk) => {
-          data += chunk;
-      });
-    
-      res.on('end', () => {
-          const response = JSON.parse(data);
-          const reviews = response.reviews;
-          
-          infer("Summarize this array of reviews into a 4 sentence summary. "+ reviews);
-          
-          for (const review of reviews) {
-              // console.log(`Review by ${review.user.name}:`);
-              // console.log(`Rating: ${review.rating}`);
-              console.log(`Review: ${review.text}\n`);
-          }
-      });
-  });
-    
-  req.on('error', (error) => {
-      console.error('Error:', error.message);
-  });
-      
-  req.end();
-}
